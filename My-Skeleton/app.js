f06e0ebac25b21c1b4148cdf4f25ceda6103f4a0
Hristo Yonkov <hristopturs@gmail.com>
2022-07-11 10:15:18 +0300

    Update app.js

--- My-Skeleton/app.js
+++ My-Skeleton/app.js
@@ -3,26 +3,17 @@ import { navView } from "./src/middlewares/nav.js";
 
 
 import { clearUser, getToken } from "./src/services/userService.js";
-import { catalogView } from "./src/views/catalog.js";
-import { createView } from "./src/views/create.js";
-import { detailsView } from "./src/views/details.js";
 import { editView } from "./src/views/edit.js";
-import { homeView } from "./src/views/home.js";
 import { loginView } from "./src/views/login.js";
-import { profileView } from "./src/views/profile.js";
 import { registerView } from "./src/views/register.js";
 
 page(navView);
 
-page('/', homeView)
 page('/login', loginView)
 page('/register', registerView)
-page('/catalog', catalogView)
-page('/details/:id', detailsView)
 page('/logout', onLogout)
 page('/edit/:id', editView)
-page('/create', createView)
-page('/profile', profileView)
+
 
 
 page.start();
